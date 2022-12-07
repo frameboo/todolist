552f095d15fa20d456c4d82cc478d6b9f9bb0c70
frame <frame.boo@gmail.com>
2022-12-07 17:16:40 +0300

    33 homework done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from core.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (
+            None,
+            {'fields': ('username', 'password')}
+        ),
+        (
+            'Personal info',
+            {'fields': ('first_name', 'last_name', 'email')}
+        ),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            'Other info',
+            {'fields': ('last_login', 'date_joined')}
+        ),
+    )
+
+
+admin.site.unregister(Group)
--- todolist/settings.py
+++ todolist/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -108,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'core.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
