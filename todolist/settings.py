7402b86c42f5d94fedd45d7718cbd50efb9ea8a0
frame <frame.boo@gmail.com>
2023-01-18 19:25:21 +0300

    actions

--- todolist/settings.py
+++ todolist/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
